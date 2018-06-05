19d7ba81154a549ac0b11735f80e13ffded07c1f
bashir <bashir.abihabib@gmail.com>
2018-06-05 19:44:23 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+$('.nav-link').hover(function() {
+    $(this).closest('.nav-item.dropdown').siblings('.nav-item.dropdown').removeClass('show').find('.dropdown-menu').removeClass('show');
+})
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
